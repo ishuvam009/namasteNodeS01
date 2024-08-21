ffd1afe199489b7836596cb6728b74530a53d15d
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-08-21 19:48:12 +0530

    Commit

--- Exports/app.js
+++ Exports/app.js
@@ -1,4 +1,10 @@
-const {multiply,sum} = require('../Day3/')
+const {multiply,sum} = require('../Day3')
+const data = require('./data.json')
 
-const result = multiply(15,2);
-console.log(result);
+const result1 = multiply(15,2);
+const result2 = sum(99,1);
+
+
+console.log(JSON.stringify(data));
+console.log(result1);
+console.log(result2);
